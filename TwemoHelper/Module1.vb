38ba8479ef5402b5f8dce9df82724b2831748559
rambopfc <JoeBotta@gmail.com>
2015-12-14 23:27:34 -0500

    Added "emoticons" writer + file check

--- TwemoHelper/Module1.vb
+++ TwemoHelper/Module1.vb
@@ -4,8 +4,10 @@ Module Module1
     Sub Main()
         Try
             Dim CurrentFolder As String = Path.GetDirectoryName(Process.GetCurrentProcess.MainModule.FileName)
-            Dim AllTheFiles As String() = IO.Directory.GetFiles(CurrentFolder,"*.tga")
-            Dim SingleFile As String
+            Dim AllTheFiles As String() = IO.Directory.GetFiles(CurrentFolder, "*.tga")
+            If AllTheFiles.Length = 0 Then
+                Throw New System.Exception("There are no tga files in this folder")
+            End If
             If File.Exists("output.txt") Then
                 File.Delete("output.txt")
                 File.Create("output.txt").Close()
@@ -13,18 +15,24 @@ Module Module1
                 File.Create("output.txt").Close()
             End If
 
-            'This Is AllTheFiles defaultpack writer
-            For Each SingleFile In AllTheFiles
-                Dim x As String = Path.GetFileName(SingleFile).ToString()
+            'This Is AllTheFiles "defaultpack" writer
+            For Each SingleFile As String In AllTheFiles
                 Using w As StreamWriter = File.AppendText("output.txt")
                     w.WriteLine("[""" & Path.GetFileName(SingleFile).ToString & """]=""Interface\\AddOns\\twemo\\" & Path.GetFileName(SingleFile).ToString & ":32:32"",")
-
+                    If Array.IndexOf(AllTheFiles, SingleFile) = Array.IndexOf(AllTheFiles, AllTheFiles.Last) Then
+                        w.WriteLine("----------------------------------------------------------")
+                        w.WriteLine("----------------------------------------------------------")
+                    End If
                 End Using
-
-
-
-
             Next
+
+            'This is AllTheFiles "emoticons" writer
+            For Each SingleFile As String In AllTheFiles
+                Using w As StreamWriter = File.AppendText("output.txt")
+                    w.WriteLine("[""" & Path.GetFileNameWithoutExtension(SingleFile) & """]=""" & Path.GetFileNameWithoutExtension(SingleFile) & """,")
+                End Using
+            Next
+
         Catch ex As Exception
             Console.WriteLine(ex.ToString)
             Console.ReadKey(True)
