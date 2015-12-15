d0f1f6ca5ef761fd1cd77cccfc436303f96abab8
rambopfc <JoeBotta@gmail.com>
2015-12-14 22:00:55 -0500

    Added file logic

--- TwemoHelper/Module1.vb
+++ TwemoHelper/Module1.vb
@@ -2,7 +2,13 @@
 Module Module1
 
     Sub Main()
+        Dim CurrentFolder As String = Path.GetDirectoryName(Process.GetCurrentProcess.MainModule.FileName)
 
+        Dim AllTheFiles As String() = IO.Directory.GetFiles(CurrentFolder)
+        Dim SingleFile As String
+        For Each SingleFile In AllTheFiles
+            Dim x As String = Path.GetFileName(SingleFile).ToString()
+        Next
 
     End Sub
 
