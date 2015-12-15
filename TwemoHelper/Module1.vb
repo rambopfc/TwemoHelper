82c50344de5d145a01975d0da1324dcfbe23c582
rambopfc <JoeBotta@gmail.com>
2015-12-14 23:04:39 -0500

    added "defualtpack" writer

--- TwemoHelper/Module1.vb
+++ TwemoHelper/Module1.vb
@@ -2,14 +2,36 @@
 Module Module1
 
     Sub Main()
-        Dim CurrentFolder As String = Path.GetDirectoryName(Process.GetCurrentProcess.MainModule.FileName)
+        Try
+            Dim CurrentFolder As String = Path.GetDirectoryName(Process.GetCurrentProcess.MainModule.FileName)
+            Dim AllTheFiles As String() = IO.Directory.GetFiles(CurrentFolder,"*.tga")
+            Dim SingleFile As String
+            If File.Exists("output.txt") Then
+                File.Delete("output.txt")
+                File.Create("output.txt").Close()
+            Else
+                File.Create("output.txt").Close()
+            End If
 
-        Dim AllTheFiles As String() = IO.Directory.GetFiles(CurrentFolder)
-        Dim SingleFile As String
-        For Each SingleFile In AllTheFiles
-            Dim x As String = Path.GetFileName(SingleFile).ToString()
-        Next
+            'This Is AllTheFiles defaultpack writer
+            For Each SingleFile In AllTheFiles
+                Dim x As String = Path.GetFileName(SingleFile).ToString()
+                Using w As StreamWriter = File.AppendText("output.txt")
+                    w.WriteLine("[""" & Path.GetFileName(SingleFile).ToString & """]=""Interface\\AddOns\\twemo\\" & Path.GetFileName(SingleFile).ToString & ":32:32"",")
+
+                End Using
+
+
+
+
+            Next
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+            Console.ReadKey(True)
+        End Try
+
+
+
 
     End Sub
-
 End Module
